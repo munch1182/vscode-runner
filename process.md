34b1217e7dba0d29a7547bf3533764698dddb28e
munch1182 <munch1182@qq.com>
2024-09-01 01:47:02 +0800

    create extensions

---
+++ process.md
@@ -0,0 +1,9 @@
+# vscode extensions
+
+[官方指导](https://code.visualstudio.com/api/get-started/your-first-extension)
+
+## 创建并测试
+
+1. 安装`npm i -g yo generator-code`
+2. 运行`yo code`并选择对应选项
+3. 打开项目并点击（`run/start debugging`，默认按键`F5`）测试
--- src/extension.ts
+++ src/extension.ts
@@ -8,12 +8,12 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-runner" is now active!');
+	console.log('Congratulations, your extension "munch1182-vscode-runner" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-runner.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('munch1182-vscode-runner.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from vscode-runner!');
