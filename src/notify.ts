5153511fdf96a867f96c061e83442aaa5dfe8d23
munch1182 <munch1182@qq.com>
2024-09-01 23:33:43 +0800

    project run

--- src/notify.ts
+++ src/notify.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
 
 export function notifyErr(err: string) {
-  vscode.window.showErrorMessage(err);
+  vscode.window.showInformationMessage(err);
 }
--- src/runner.ts
+++ src/runner.ts
@@ -2,10 +2,12 @@ import * as vscode from "vscode";
 import log from "./log";
 import path from "path";
 import { notifyErr } from "./notify";
+import * as fs from "fs";
 
 const NAME_CONFIG_SECTION = "vscode-runner";
 const NAME_CONFIG_FILE_RUNNER = "fileRunner";
 const NAME_TERMINAL = "runner";
+const EXCLUDE_DIR = [".git", "node_modules", "dist", "build", "out", "debug"];
 
 export class Runner {
   // 文件所在文件夹
@@ -15,7 +17,10 @@ export class Runner {
   // 文件名，不含拓展，当传入文件夹时为空
   readonly name?: string;
 
+  readonly uri: vscode.Uri;
+
   constructor(uri: vscode.Uri) {
+    this.uri = uri;
     const filePath = uri.fsPath;
     this.dir = path.dirname(filePath);
     this.ext = path.extname(filePath);
@@ -36,7 +41,7 @@ export class Runner {
   }
 
   run() {
-    log("run: " + this.filePath());
+    log("run in: " + this.filePath());
   }
 
   /**
@@ -140,10 +145,129 @@ export class FileRunner extends Runner {
 /**
  * 运行当前页面所在的项目的运行命令
  */
-export class ProjectRunner extends Runner {}
+export class ProjectRunner extends Runner {
+  run() {
+    super.run();
+    if (!this.checkValue()) {
+      notifyErr("cannot run this project");
+      return;
+    }
+    const dir = vscode.workspace.getWorkspaceFolder(this.uri)?.uri.fsPath;
+    if (!dir) {
+      notifyErr("cannot find project");
+      return;
+    }
+    const project = Project.buildProject(dir);
+    if (!project) {
+      notifyErr("cannot find project");
+      return;
+    }
+    const cmd = project.getRunCMD();
+    this.execCmd(cmd);
+  }
+}
 
-class NodeRunner extends ProjectRunner {}
+class Project {
+  // 判断项目文件夹
+  readonly dir!: string;
 
-class RustRunner extends ProjectRunner {}
+  constructor(dir: string) {
+    this.dir = dir;
+  }
 
-class TauriRunner extends NodeRunner {}
+  check(): boolean {
+    throw new Error("not implemented");
+  }
+
+  getRunCMD(): string {
+    throw new Error("not implemented");
+  }
+
+  static buildProject(dir: string): Project | undefined {
+    // 判断是有顺序的，因为tauri包含rust项目和node项目
+    // 所以，这3个判断合在一处
+    const project = TauriProject.buildNodeRustProject(dir);
+    if (project !== undefined) {
+      return project;
+    }
+    // other
+    return undefined;
+  }
+}
+
+class NodeProject extends Project {
+  static isProject(dir: string): boolean {
+    const pkgJson = find(dir, "package.json");
+    log("find: " + pkgJson);
+    if (!pkgJson) {
+      return false;
+    }
+    return true;
+  }
+
+  getRunCMD(): string {
+    return "npm run dev";
+  }
+}
+
+class TauriProject extends Project {
+  static buildNodeRustProject(dir: string): Project | undefined {
+    const node = NodeProject.isProject(dir);
+    const rust = RustProject.isProject(dir);
+    log("project: node: " + node + ", rust: " + rust);
+    if (node && rust) {
+      return new TauriProject(dir);
+    }
+    if (node) {
+      return new NodeProject(dir);
+    }
+    if (rust) {
+      return new RustProject(dir);
+    }
+    return undefined;
+  }
+
+  getRunCMD(): string {
+    return "npm run tauri dev";
+  }
+}
+class RustProject extends Project {
+  static isProject(dir: string): boolean {
+    const cargoToml = find(dir, "Cargo.toml");
+    log("find: " + cargoToml);
+    if (!cargoToml) {
+      return false;
+    }
+    return true;
+  }
+
+  getRunCMD(): string {
+    return "cargo run";
+  }
+}
+
+/**
+ * 在 dir 文件夹及其子文件夹中查找名为 file 的文件，成功则返回该文件的路径
+ */
+function find(dir: string, file: string): string | undefined {
+  if (!fs.existsSync(dir)) {
+    return undefined;
+  }
+  if (!fs.lstatSync(dir).isDirectory()) {
+    return path.basename(dir) === file ? dir : undefined;
+  }
+  const dirname = path.basename(dir);
+  if (EXCLUDE_DIR.includes(dirname)) {
+    return undefined;
+  }
+  const dirs = fs.readdirSync(dir);
+  for (const item of dirs) {
+    const _path = `${dir}${path.sep}${item}`;
+    const result = find(_path, file);
+    if (result) {
+      return result;
+    }
+  }
+
+  return undefined;
+}
